c572131049177bbc432779a9ec4d913649e06f25
Shell He <she2019@kewforest.org>
2017-11-28 14:33:40 -0500

    translator

---
+++ Untitled.py
@@ -0,0 +1,161 @@
+#Translation
+print("Welcome to use 'TRANSLATION'")
+print("Notice: ")
+print("    If you are typing English to Chinese")
+print("    No Capital or quotation mark")
+
+a = input("Translation: ")
+
+if a == ("早上好"):
+    print("Good morning!")    
+if a ==("good morning"):
+    print("早上好！")
+
+if a ==("再见"):
+    print("Good bye!")
+if a==("good bye"):
+    print("再见")
+    
+if a ==("晚安"):
+    print("Good night!")
+if a==("good night"):
+    print("晚安")
+    
+if a==("谢谢"):
+    print("Thank you!")
+if a==("thank you"):
+    print("谢谢")
+
+if a==("太好了"):
+    print("Wonderful!")
+if a==("wonderful"):
+    print("太好了")
+    
+if a==("多亏有你帮忙"):
+    print("Thanks for your help!")
+if a==("thank for your help"):
+    print("多亏有你帮忙")
+
+if a==("太奇妙了"):
+    print("That is amazing!")
+if a ==("that is amazing"):
+    print("太奇妙了")
+
+if a==("你学的真快"):
+    print("You are learning so fast!")
+if a ==("you are learning so fast"):
+    print("你学的真快啊")
+    
+if a==("你真棒"):
+    print("You r great!")
+if a ==("you are great"):
+    print("你真棒")
+
+if a==("你真能干"):
+    print("You are really talented!")
+if a ==("you are really talented"):
+    print("你真能干")
+
+if a==("我真为你高兴"):
+    print("I am happy for you")
+if a ==("i am happy for you"):
+    print("我真为你高兴")
+
+if a==("我好羡慕你"):
+    print("I envy you so much")
+if a ==("i envy you so much"):
+    print("我好羡慕你")
+
+if a==("我很可爱"):
+    print("I am so cute! QWQ")
+if a ==("i am so cute"):
+    print("我很可爱 QWQ")
+
+if a==("你好可爱"):
+    print("You are so cute o3o")
+if a ==("you are so cute"):
+    print("你好可爱 o3o")
+    
+if a==("你好漂亮"):
+    print("You are so beautiful 0.0")
+if a ==("you are so beautiful"):
+    print("你好漂亮 0.0")
+        
+if a==("你好帅"):
+    print("You are so handsome! QAQ")
+if a ==("you are so handsome"):
+    print("你好帅！ QAQ")
+    
+        
+if a==("超性感"):
+    print("So sexy! $.$")
+if a ==("so sexy"):
+    print("超性感哦 $.$")
+
+if a==("我错了"):
+    print("I am sorry")
+if a ==(" i am sorry"):
+    print("我错了")
+    
+if a==("我来帮助你好吗"):
+    print("May I help you?")
+if a ==("may i help you"):
+    print("我来帮助你好吗？")
+        
+if a==("帮帮我好吗"):
+    print("Can you help me?")
+if a ==("can you help me"):
+    print("帮帮我好吗")
+    
+if a==("你真会思考"):
+    print("You are smart")
+if a ==("you are smart"):
+    print("你真会思考哦")
+        
+if a==("不怕 我们来想想办法"):
+    print("Do not worry, let's think about it")
+if a ==("do not worry let's think about it"):
+    print("不怕 我们来想想办法")
+    
+if a==("有进步"):
+    print("You get progress!")
+if a ==("you get progress"):
+    print("有进步")
+        
+if a==("再试一次"):
+    print("Try again!")
+if a ==("try again"):
+    print("再试一次")
+    
+if a==("你真有理想"):
+    print("You have a great ideal")
+if a ==("you have a great ideal"):
+    print("你真有理想")
+        
+if a==("和你在一起 真快乐"):
+    print("I am so happy with you")
+if a ==("i am so happy with you"):
+    print("和你在一起 真快乐")
+ 
+    
+else:
+    print("Sorry, alpaca ate the word")
+    print("┏┛┻━━━┛┻┓")
+    print("┃｜｜｜｜｜｜｜┃")
+    print("┃　　　━　　　┃")
+    print("┃ ┳┛ 　┗┳ ┃")
+    print("┃　　　　　　　┃")
+    print("┃　　　┻　　　┃")
+    print("┃　　　P 　　　┃")
+    print("┗━┓　a  ┏━┛")
+    print("　　┃　t  ┃　　")
+    print("　　┃  r  ┃　　")
+    print("　　┃　i  ┃　　")
+    print("　　┃　c  ┃")
+    print("　　┃　h  ┗━━━┓")
+    print("　　┃ o         ┣┓")
+    print("　　┃ r         ┃")
+    print("　　┗┓┓┏━┳┓┏┛")
+    print("　　　┃┫┫　┃┫┫")
+    print("　　　┗┻┛　┗┻┛")
+    
