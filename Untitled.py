829268ca7548ef5a8ffd3a99839fdbfd0f440702
Shell He <she2019@kewforest.org>
2018-01-12 12:38:08 -0500

    jjj

--- Untitled.py
+++ Untitled.py
@@ -1,161 +1,34 @@
 #Translation
-print("Welcome to use 'TRANSLATION'")
+translations = [("早上好","Good morning"), ("再见","Good bye"),("谢谢","Thank you"),("晚安","Goodnight"),("太好了","Wonderful!"),("多亏有你帮忙","Thanks for your help!"),("太奇妙了","that's amazing"),("你学得真快","you are learning so fast"),("你真棒","You are great!"),("你真能干","you are pretty talented"),("我真为你高兴","I'm happy for you"),("我好羡慕你","I envy you so much"),("你好可爱","you’re so cute"),("我妈说我很特别","moms says i'm special")]
+
+print("Welcome to use 'how to get a girl TRANSLATION'")
 print("Notice: ")
 print("    If you are typing English to Chinese")
 print("    No Capital or quotation mark")
 
 a = input("Translation: ")
 
-if a == ("早上好"):
-    print("Good morning!")
-elif a ==("good morning"):
-    print("早上好！")
-
-elif a ==("再见"):
-    print("Good bye!") 
-elif a==("good bye"):
-    print("再见")
+for i in translations:
+    if i[0] == a:
+        print(i[1])
     
-elif a ==("晚安"):
-    print("Good night!") 
-elif a==("good night"):
-    print("晚安")
-    
-elif a==("谢谢"):
-    print("Thank you!")
-elif a==("thank you"):
-    print("谢谢")
-
-elif a==("太好了"):
-    print("Wonderful!")
-elif a==("wonderful"):
-    print("太好了")
-    
-elif a==("多亏有你帮忙"):
-    print("Thanks for your help!")
-elif a==("thank for your help"):
-    print("多亏有你帮忙")
-
-elif a==("太奇妙了"):
-    print("That is amazing!")
-elif a ==("that is amazing"):
-    print("太奇妙了")
-
-elif a==("你学的真快"):
-    print("You are learning so fast!")
-elif a ==("you are learning so fast"):
-    print("你学的真快啊")
-    
-elif a==("你真棒"):
-    print("You r great!")
-elif a ==("you are great"):
-    print("你真棒")
-
-elif a==("你真能干"):
-    print("You are really talented!")
-elif a ==("you are really talented"):
-    print("你真能干")
-
-elif a==("我真为你高兴"):
-    print("I am happy for you")
-elif a ==("i am happy for you"):
-    print("我真为你高兴")
-
-elif a==("我好羡慕你"):
-    print("I envy you so much")
-elif a ==("i envy you so much"):
-    print("我好羡慕你")
-
-elif a==("我很可爱"):
-    print("I am so cute! QWQ")
-elif a ==("i am so cute"):
-    print("我很可爱 QWQ")
-
-elif a==("你好可爱"):
-    print("You are so cute o3o")
-elif a ==("you are so cute"):
-    print("你好可爱 o3o")
-    
-elif a==("你好漂亮"):
-    print("You are so beautiful 0.0")
-elif a ==("you are so beautiful"):
-    print("你好漂亮 0.0")
-        
-elif a==("你好帅"):
-    print("You are so handsome! QAQ")
-elif a ==("you are so handsome"):
-    print("你好帅！ QAQ")
-    
-        
-elif a==("超性感"):
-    print("So sexy! $.$")
-elif a ==("so sexy"):
-    print("超性感哦 $.$")
-
-elif a==("我错了"):
-    print("I am sorry")
-elif a ==(" i am sorry"):
-    print("我错了")
-    
-elif a==("我来帮助你好吗"):
-    print("May I help you?")
-elif a ==("may i help you"):
-    print("我来帮助你好吗？")
-        
-elif a==("帮帮我好吗"):
-    print("Can you help me?")
-elif a ==("can you help me"):
-    print("帮帮我好吗")
-    
-elif a==("你真会思考"):
-    print("You are smart")
-elif a ==("you are smart"):
-    print("你真会思考哦")
-        
-elif a==("不怕我们来想想办法"):
-    print("Do not worry, let's think about it")
-elif a ==("do not worry let's think about it"):
-    print("不怕 我们来想想办法")
-    
-elif a==("有进步"):
-    print("You get progress!")
-elif a ==("you get progress"):
-    print("有进步")
-        
-elif a==("再试一次"):
-    print("Try again!")
-elif a ==("try again"):
-    print("再试一次")
-    
-elif a==("你真有理想"):
-    print("You have a great ideal")
-elif a ==("you have a great ideal"):
-    print("你真有理想")
-        
-elif a==("和你在一起 真快乐"):
-    print("I am so happy with you")
-elif a ==("i am so happy with you"):
-    print("和你在一起 真快乐")
- 
-    
-else:
-    print("Alpaca ate the word")
-    print("┏┛┻━━━┛┻┓")
-    print("┃｜｜｜｜｜｜｜┃")
-    print("┃　　　━　　　┃")
-    print("┃ ┳┛ 　┗┳ ┃")
-    print("┃　　　　　　　┃")
-    print("┃　　　┻　　　┃")
-    print("┃　　　P 　　　┃")
-    print("┗━┓　a  ┏━┛")
-    print("　　┃　t  ┃　　")
-    print("　　┃  r  ┃　　")
-    print("　　┃　i  ┃　　")
-    print("　　┃　c  ┃")
-    print("　　┃　h  ┗━━━┓")
-    print("　　┃ o         ┣┓")
-    print("　　┃ r         ┃")
-    print("　　┗┓┓┏━┳┓┏┛")
-    print("　　　┃┫┫　┃┫┫")
-    print("　　　┗┻┛　┗┻┛")
-    
+    else:
+       print("llama ate the word")
+       print("┏┛┻━━━┛┻┓")
+       print("┃｜｜｜｜｜｜｜┃")
+       print("┃　　　━　　　┃")
+       print("┃ ┳┛ 　┗┳ ┃")
+       print("┃　　　　　　　┃")
+       print("┃　　　┻　　　┃")
+       print("┃　　　P 　　　┃")
+       print("┗━┓　a  ┏━┛")
+       print("　　┃　t  ┃　　")
+       print("　　┃  r  ┃　　")
+       print("　　┃　i  ┃　　")
+       print("　　┃　c  ┃")
+       print("　　┃　h  ┗━━━┓")
+       print("　　┃ o         ┣┓")
+       print("　　┃ r         ┃")
+       print("　　┗┓┓┏━┳┓┏┛")
+       print("　　　┃┫┫　┃┫┫")
+       print("　　　┗┻┛　┗┻┛")
