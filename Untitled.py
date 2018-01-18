f3f26a24078990bfdbade8ab46e6b0189c77d55e
Shell He <she2019@kewforest.org>
2018-01-18 14:58:50 -0500

    hhh

--- Untitled.py
+++ Untitled.py
@@ -1,18 +1,25 @@
 #Translation
-translations = [("早上好","Good morning"), ("再见","Good bye"),("谢谢","Thank you"),("晚安","Goodnight"),("太好了","Wonderful!"),("多亏有你帮忙","Thanks for your help!"),("太奇妙了","that's amazing"),("你学得真快","you are learning so fast"),("你真棒","You are great!"),("你真能干","you are pretty talented"),("我真为你高兴","I'm happy for you"),("我好羡慕你","I envy you so much"),("你好可爱","you’re so cute"),("我妈说我很特别","moms says i'm special")]
+translations = [("早上好","Good morning"),("再见","Good bye"),("谢谢","Thank you"),("晚安","Goodnight"),("太好了","Wonderful!"),("多亏有你帮忙","Thanks for your help!"),("太奇妙了","that's amazing"),("你学得真快","you are learning so fast"),("你真棒","You are great!"),("你真能干","you are pretty talented"),("我真为你高兴","I'm happy for you"),("我好羡慕你","I envy you so much"),("你好可爱","you’re so cute"),("我妈说我很特别","moms says i'm special")]
+#translations = [("Good morning",""), ("Goodnight",""),("Thank you",""),("You are so cute",""),("You are so sexy",""),("Can I get your number",""),("Can I g
 
-print("Welcome to use 'how to get a girl TRANSLATION'")
-print("Notice: ")
-print("    If you are typing English to Chinese")
-print("    No Capital or quotation mark")
+def intochinese():
+    a = input("Translation: ")
+    for i in translations:
+        if i[0] == a:
+            print(i[1])
+        else:
+            llama()
 
-a = input("Translation: ")
 
-for i in translations:
-    if i[0] == a:
-        print(i[1])
-    
-    else:
+def intoenglish():
+    a = input("Translation: ")
+    for i in translations:
+        if i[1] == a:
+            print(i[0])
+        else:
+            llama()
+          
+def llama():
        print("llama ate the word")
        print("┏┛┻━━━┛┻┓")
        print("┃｜｜｜｜｜｜｜┃")
@@ -32,3 +39,16 @@ for i in translations:
        print("　　┗┓┓┏━┳┓┏┛")
        print("　　　┃┫┫　┃┫┫")
        print("　　　┗┻┛　┗┻┛")
+            
+print("Welcome to use 'how to get a girl TRANSLATION'")
+print("Notice: ")
+print(" English to Chinese or Chinese to English?")
+print("    No Capital or quotation mark")
+
+
+language = input("English or Chinese")
+if language == "English":
+    intochinese()
+else:
+    intoenglish()
+
